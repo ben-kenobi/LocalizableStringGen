fd128c9bc2d202f4f0a83a27ce757f731a0e7876
hui <hui@huideMacBook-Pro.local>
2018-12-18 23:30:02 +0800

    更新pod

---
+++ LocalizableStringGen/Pods/IOS_OC_BASIC/IOS_OC_BaseKit/UIS/YFNoHitVC.m
@@ -0,0 +1,21 @@
+
+
+//
+//  YFNoHitVC.m
+//  IOS_OC_BASIC
+//
+//  Created by yf on 2018/12/18.
+//  Copyright © 2018 yf. All rights reserved.
+//
+
+#import "YFNoHitVC.h"
+#import "YFNoHitView.h"
+
+
+@implementation YFNoHitVC
+
+-(void)loadView{
+    self.view = [[YFNoHitView alloc]init];
+}
+
+@end
