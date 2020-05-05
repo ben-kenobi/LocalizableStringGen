ded6a5fd9586f8d64774153e7cd393d023f6df05
ben-kenobi <whatelsecani@gmail.com>
2020-05-06 00:56:29 +0800

    增加日语

--- LocalizableStringGen/LocalizableStringGen/main/C/YFStringsUtilVC.m
+++ LocalizableStringGen/LocalizableStringGen/main/C/YFStringsUtilVC.m
@@ -57,8 +57,8 @@
 }
 
 -(void)doCSVCommonFlow{
-    int valIdxes[] = {2,3,4,5,6,7,8,9};
-    NSString *valTitle[] = {@"EN",@"CN",@"ES",@"FR",@"DE",@"IT",@"NL",@"AR"};
+    int valIdxes[] = {2,3,4,5,6,7,8,9,10};
+    NSString *valTitle[] = {@"EN",@"CN",@"ES",@"FR",@"DE",@"IT",@"NL",@"AR",@"JP"};
     
     dispatch_semaphore_t sema = dispatch_semaphore_create(0);
     for(int i=0;i<sizeof(valIdxes)/sizeof(int);i++){
